c1e0b1799ea35dee5367554fb7032f7287adde19
Ethan Lee <ethantlee21@gmail.com>
2025-05-25 14:15:13 -0400

    update items schema and data

--- src/types/item.d.ts
+++ src/types/item.d.ts
@@ -7,6 +7,8 @@ export type Item = {
   price: number;
   postedAt: string;
   isSold: boolean;
+  condition: number;
+  isNegotiable: boolean;
   views: number;
   condition: number;
   subcategory: string;
